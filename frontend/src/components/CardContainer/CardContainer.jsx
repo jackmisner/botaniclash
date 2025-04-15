be2b40e4afb6f09acccf26ce44aa89264cf82801
Michal <m.podolak14@gmail.com>
2025-04-15 17:14:19 +0100

    feat: initial card distribution logic

--- frontend/src/components/CardContainer/CardContainer.jsx
+++ frontend/src/components/CardContainer/CardContainer.jsx
@@ -1,11 +1,17 @@
 import { Card } from "../Card/Card";
 
-export const CardContainer = ({ plants }) => {
+export const CardContainer = ({ plants, onClickHandle, setOpeningHand }) => {
     return (
         <div className="cards-container">
-            {plants.map((plant) => (
-                <Card key={plant.id} plant={plant} />
-            ))}
+            {plants.length > 0 &&
+                plants.map((plant) => (
+                    <Card
+                        setOpeningHand={setOpeningHand}
+                        onClick={onClickHandle}
+                        key={plant.id}
+                        plant={plant}
+                    />
+                ))}
         </div>
     );
 };
