dc8d01775e768635500789363e12bce75050ae5c
Luke Howeth <LukeHoweth30@gmail.com>
2025-04-15 15:04:18 +0100

    code so far

--- api/src/models/plant.go
+++ api/src/models/plant.go
@@ -4,5 +4,15 @@ import "gorm.io/gorm"
 
 type Plant struct {
 	gorm.Model
-	CommonName string `"json":"`
+	CommonName          string `json:"common_name"`
+	ScientificName      string `json:"scientific_name"`
+	ImageUrl            string `json:"image_url"`
+	Year                int    `json:"year"`
+	Observations        string `json:"observations"`
+	Edible              bool   `json:"edible"`
+	PhMinium            int    `json:"ph_minimum"`
+	PhMaxiumum          int    `json:"ph_maximum"`
+	Light               int    `json:"light"`
+	SoilNutriments      int    `json:"soil_nutriments"`
+	AtmosphericHumidity int    `json:"atmospheric_humidity"`
 }
