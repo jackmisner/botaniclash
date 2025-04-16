971228642a6de195331bb5307919a72846ac67b2
abbiefinlayson1 <abbiefinlayson@monzo.com>
2025-04-16 15:40:40 +0100

    added set up plant route to routes.go, corrected maximum spelling error in plantseed.go

--- api/src/routes/routes.go
+++ api/src/routes/routes.go
@@ -4,6 +4,7 @@ import "github.com/gin-gonic/gin"
 
 func SetupRoutes(engine *gin.Engine) {
 	apiRouter := engine.Group("/")
+	setupPlantRoutes(apiRouter) // sets up the plant routes
 	setupUserRoutes(apiRouter)
 	setupPostRoutes(apiRouter)
 	setupAuthenticationRoutes(apiRouter)
