01b087ccbdaee8fc066ffc4d791208b93172eca9
ImogenLovell <imogen.lovell@yahoo.co.uk>
2025-04-16 14:09:18 +0100

    Updated token controller to use FindUserByUsername function, added comments to user_test, added binary to gitignore

--- api/src/models/user_test.go
+++ api/src/models/user_test.go
@@ -8,6 +8,8 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+// This re useable function gets the test database from the .test.env file, 
+// uses it to open a DB connection and then autmigrates all tables to get ready for the test
 func OpenConnection()  {
 	err := godotenv.Load("../../.test.env")
 	if err != nil {
@@ -17,19 +19,20 @@ func OpenConnection()  {
 	AutoMigrateModels()
 }
 
+// Creates and saves a user, then checks if the user can be retrieved by Username
 func TestFindUserbyUsername(t *testing.T)  {
-	OpenConnection()
-	testuser := &User{
+	OpenConnection()  //Call open connection function
+	testuser := &User{  //Setup testuser
 		Username: "GardenLover", 
 		Password: "123Plant"}
 	
-	savedUser, err := testuser.Save()
+	savedUser, err := testuser.Save()  //Save test user 
 	require.NoError(t, err)
 
-	fetchedUser, err := FindUserByUsername(savedUser.Username)
+	fetchedUser, err := FindUserByUsername(savedUser.Username) //Retrieve that user
 	require.NoError(t, err)
 
-	assert.Equal(t, savedUser.Username, fetchedUser.Username)
+	assert.Equal(t, savedUser.Username, fetchedUser.Username)  //Compare the user we created to the data we retreieved
 	assert.Equal(t, savedUser.Password, fetchedUser.Password) // This may need to be rewritten if password hashing is introduced
 
 }
