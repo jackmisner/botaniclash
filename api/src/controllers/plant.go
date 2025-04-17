b18da54301b0e5b4767ddc4e3961c75ac50b89ba
Luke Howeth <LukeHoweth30@gmail.com>
2025-04-17 16:13:00 +0100

    GET /plants now returns the plant ID too

--- api/src/controllers/plant.go
+++ api/src/controllers/plant.go
@@ -16,6 +16,7 @@ type PlantCards struct {
 
 // PlantData represents detailed information about a plant.
 type PlantData struct {
+	ID                  uint     `json:"id"`
 	CommonName          string   `json:"common_name"`
 	ScientificName      string   `json:"scientific_name"`
 	ImageUrl            string   `json:"image_url"`
@@ -43,32 +44,32 @@ func GetAllPlants(c *gin.Context) {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to fetch plants"})
 		return
 	}
-	
-	r := rand.New(rand.NewSource(time.Now().UnixNano())) // Create a new random number generator seeded with the current time in nanoseconds. 
+
+	r := rand.New(rand.NewSource(time.Now().UnixNano())) // Create a new random number generator seeded with the current time in nanoseconds.
 	r.Shuffle(len(plants), func(i, j int) {
 		plants[i], plants[j] = plants[j], plants[i]
-	}	) // Shuffle the plants slice in place using the random number generator.
+	}) // Shuffle the plants slice in place using the random number generator.
 
 	var randomCards []PlantData
-	for i := 0; i < 20 && i < len(plants); i++ { // Selects the first 20 plants from the shuffled slice 
-		p := plants[i] // Creating a new plant object for each plant in the slice 
-		phRange := p.PhMaximum - p.PhMinimum // Calculating the pH range by subtracting the minimum pH from the maximum pH
-		phAverage := float64(p.PhMinimum + p.PhMaximum) / 2.0 // Calculating the average pH by adding the minimum and maximum pH values and dividing by 2.0 
+	for i := 0; i < 20 && i < len(plants); i++ { // Selects the first 20 plants from the shuffled slice
+		p := plants[i]                                      // Creating a new plant object for each plant in the slice
+		phRange := p.PhMaximum - p.PhMinimum                // Calculating the pH range by subtracting the minimum pH from the maximum pH
+		phAverage := float64(p.PhMinimum+p.PhMaximum) / 2.0 // Calculating the average pH by adding the minimum and maximum pH values and dividing by 2.0
 
-		randomCards = append(randomCards, PlantData{ // Creates a new plant object and adds it to the randomcard slice 
-			CommonName:          p.CommonName,
-			ScientificName:      p.ScientificName,
-			ImageUrl:            p.ImageUrl,
-			Year:                p.Year,
-			Observations:        p.Observations,
-			Edible:              p.Edible,
-			PhLevels:
-				PhLevels{
-					PhMinimum: p.PhMinimum,
-					PhMaximum: p.PhMaximum,
-					PhRange:   phRange,
-					PhAverage: phAverage,
-				},
+		randomCards = append(randomCards, PlantData{ // Creates a new plant object and adds it to the randomcard slice
+			ID:             p.ID,
+			CommonName:     p.CommonName,
+			ScientificName: p.ScientificName,
+			ImageUrl:       p.ImageUrl,
+			Year:           p.Year,
+			Observations:   p.Observations,
+			Edible:         p.Edible,
+			PhLevels: PhLevels{
+				PhMinimum: p.PhMinimum,
+				PhMaximum: p.PhMaximum,
+				PhRange:   phRange,
+				PhAverage: phAverage,
+			},
 			Light:               p.Light,
 			SoilNutriments:      p.SoilNutriments,
 			AtmosphericHumidity: p.AtmosphericHumidity,
@@ -76,10 +77,6 @@ func GetAllPlants(c *gin.Context) {
 	}
 	response := PlantCards{ // creates a new plant cards object and adds the random cards slice to it
 		Cards: randomCards,
-	} 
+	}
 	c.JSON(http.StatusOK, response) // Sends a JSON response with the status code 200 OK alongside the plant data needed
-} 
-
-
-
-
+}
