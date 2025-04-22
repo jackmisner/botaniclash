2badc995858c0da54aeaba94fa50ced9c661335d
ImogenLovell <imogen.lovell@yahoo.co.uk>
2025-04-22 17:23:58 +0100

    working but buggy computerChooseCompetitiveStat function, also put logic to convert snake to pascal case into it's own function

--- api/src/controllers/plant.go
+++ api/src/controllers/plant.go
@@ -99,17 +99,19 @@ func ComparePlants(c *gin.Context) {
 	}
 	statToCompare := requestBody.StatToCompare
 
-	// Step 3: Checking if it is the user or computer's turn 
-	if statToCompare == "null" {
-		c.JSON(http.StatusOK, gin.H{"turn": "it's the computer's turn!"})
-		return
-	}
-
-	// Step 4: Grabbing those plants from the DB
+	
+	// Step 2: Grabbing those plants from the DB
 	playerPlant, _ := models.FetchPlantById(requestBody.PlayerCard)
 	opponentPlant, _ := models.FetchPlantById(requestBody.OpponentCard)
+	
+	// Step 3: Checking if it is the user or computer's turn 
+	if statToCompare == "null" {
+		statToCompare = computerChooseCompetitiveStat(opponentPlant)
+	}
 
-	// Step 5: Calculate who wins (or draws) using the helper function
+	
+
+	// Step 4: Calculate who wins (or draws) using the helper function
 	winner := DeterminePlantWinner(playerPlant, opponentPlant, statToCompare)
 
 	if winner == "" {
@@ -120,7 +122,7 @@ func ComparePlants(c *gin.Context) {
 		return
 	}
 
-	c.JSON(http.StatusOK, gin.H{"winner": winner})
+	c.JSON(http.StatusOK, gin.H{"winner": winner, "stat": statToCompare})
 }
 
 func DeterminePlantWinner(playerPlant, opponentPlant *models.Plant, statToCompare string) string {
@@ -145,6 +147,88 @@ func DeterminePlantWinner(playerPlant, opponentPlant *models.Plant, statToCompar
 		}
 	} else {
 		// Convert snake_case (json) to PascalCase (needed for struct field access)
+		fieldName := convertSnakeToPascal(statToCompare)
+
+		playerValue := reflect.ValueOf(*playerPlant).FieldByName(fieldName)
+		opponentValue := reflect.ValueOf(*opponentPlant).FieldByName(fieldName)
+
+		if playerValue.Int() < opponentValue.Int() {
+			return "player"
+		} else if opponentValue.Int() < playerValue.Int() {
+			return "opponent"
+		} else {
+			return "draw"
+		}
+	}
+}
+
+func computerChooseCompetitiveStat(opponentPlant *models.Plant) string {
+	// Step 0:  We're going to be using random choice twice in this function so we set it up here
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	// Step 1: Determine if plant is edible, if the plant is edible there is a random chance this will be the return value
+	if opponentPlant.Edible {
+		if r.Intn(10) % 2 == 0 {
+			return "edible"
+		}
+	}
+	// Step 2: Compare light, soil nutriments and atmospheric humidity to determine which is the lowest and therefore most competitive 
+	lowestCompValueName := bestLow(opponentPlant)
+	fieldName := convertSnakeToPascal(lowestCompValueName)
+	lowestCompValue :=reflect.ValueOf(*opponentPlant).FieldByName(fieldName)
+
+	// Step 3: Determine if the ph range is high enough to be competitive, if not the most competitive value from step 2 takes precedence
+	phRange := opponentPlant.CalculatePhRange()
+
+	if phRange > 25 && lowestCompValue.Int() >= 5 {
+		return "ph_range"
+	} else if lowestCompValueName != "null" {
+		return lowestCompValueName
+	}
+	// Step 4: Check if year is competitive, there is a random chance of it's being returned if so, otherwise return either ph range, light, soil nutriments or atmospheric humidity, depending on what was found the most competitive value 
+	if opponentPlant.Year <= 1753 {
+		return "year"
+	}
+
+	// Step 5: If all the comaprisons fail then we just give up and pick something at random
+	var randomValue string
+	possiblevalues := [6]string{"year", "edible", "light", "nutriments_required", "humidity_level", "ph_range"}
+	rand.Shuffle((3), func(i, j int) {
+		possiblevalues[i], possiblevalues[j] = possiblevalues[j], possiblevalues[i]
+	})
+
+	randomValue = possiblevalues[0]
+
+	return randomValue
+}
+
+func bestLow(opponent_card *models.Plant) string {
+	var lowest string 
+	if opponent_card.Light < opponent_card.SoilNutriments && opponent_card.Light < opponent_card.AtmosphericHumidity {     
+		// checks if light is lowest value                  
+		lowest = "light"	
+
+	} else if opponent_card.SoilNutriments < opponent_card.Light && opponent_card.SoilNutriments < opponent_card.AtmosphericHumidity{ 
+		// checks if soil nutirments is lowest value       
+		lowest = "soil_nutriments"
+
+	} else if opponent_card.AtmosphericHumidity < opponent_card.SoilNutriments && opponent_card.AtmosphericHumidity < opponent_card.Light{   
+		// checks if atmospheric humidity is lowest value
+		lowest = "atmospheric_humidity"
+
+	} else if opponent_card.Light == opponent_card.SoilNutriments && opponent_card.Light == opponent_card.AtmosphericHumidity {
+		// Checks if all fields are equal, returns one randomly if they are all equal and lower than four
+		fields := [3]string{"light", "soil_nutriments", "atmospheric_humidity"} 
+		rand.Shuffle((3), func(i, j int) {
+			fields[i], fields[j] = fields[j], fields[i]
+		})
+		lowest = fields[0]   
+	}
+
+	return lowest
+}
+
+func convertSnakeToPascal(statToCompare string) string {
+		// Convert snake_case (json) to PascalCase (needed for struct field access)
 		fieldMap := map[string]string{
 			"year":                 "Year",
 			"light":                "Light",
@@ -160,15 +244,5 @@ func DeterminePlantWinner(playerPlant, opponentPlant *models.Plant, statToCompar
 			return "" // Invalid stat
 		}
 
-		playerValue := reflect.ValueOf(*playerPlant).FieldByName(fieldName)
-		opponentValue := reflect.ValueOf(*opponentPlant).FieldByName(fieldName)
-
-		if playerValue.Int() < opponentValue.Int() {
-			return "player"
-		} else if opponentValue.Int() < playerValue.Int() {
-			return "opponent"
-		} else {
-			return "draw"
-		}
-	}
+		return fieldName
 }
