d3eca8cd16447f23af95cea17050e3a822a27894
Luke Howeth <LukeHoweth30@gmail.com>
2025-04-22 12:40:27 +0100

    Added middleware protection to to GET and POST /plants

--- api/src/routes/plants.go
+++ api/src/routes/plants.go
@@ -3,12 +3,12 @@ package routes
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/makersacademy/go-react-acebook-template/api/src/controllers"
-	// "github.com/makersacademy/go-react-acebook-template/api/src/middleware"
+	"github.com/makersacademy/go-react-acebook-template/api/src/middleware"
 )
 
 func setupPlantRoutes(baseRouter *gin.RouterGroup) { // sets up the plant routes
 	plants := baseRouter.Group("/plants") // creates a new router group for the plants route
 
-	plants.GET("", controllers.GetAllPlants) // gets all plants
-	plants.POST("", controllers.ComparePlants)
+	plants.GET("", middleware.AuthenticationMiddleware, controllers.GetAllPlants) // gets all plants
+	plants.POST("", middleware.AuthenticationMiddleware, controllers.ComparePlants)
 }
