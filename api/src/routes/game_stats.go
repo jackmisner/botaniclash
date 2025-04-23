ef25dcaed7c6342bd6cd353419a5bcbe7507f50f
Luke Howeth <LukeHoweth30@gmail.com>
2025-04-23 15:30:46 +0100

    Added middleware to the POST /game_stats route

--- api/src/routes/game_stats.go
+++ api/src/routes/game_stats.go
@@ -3,12 +3,13 @@ package routes
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/makersacademy/go-react-acebook-template/api/src/controllers"
+	"github.com/makersacademy/go-react-acebook-template/api/src/middleware"
 )
 
 func setupGameStatsRoutes(baseRouter *gin.RouterGroup) {
 	gameStatsController := &controllers.GameStatsController{}
 	gameStats := baseRouter.Group("/game_stats")
 
-	gameStats.POST("", gameStatsController.UpdateGameStats)
+	gameStats.POST("", middleware.AuthenticationMiddleware, gameStatsController.UpdateGameStats)
 	gameStats.GET("", gameStatsController.GetAllGameStats)
 }
