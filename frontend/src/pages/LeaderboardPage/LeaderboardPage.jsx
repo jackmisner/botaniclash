208933f6ed73c70b5b809bdf4e680420f01dd4e7
Jack Misner <jack.misner@gmail.com>
2025-04-23 11:32:13 +0100

    Add css to match rest of app

--- frontend/src/pages/LeaderboardPage/LeaderboardPage.jsx
+++ frontend/src/pages/LeaderboardPage/LeaderboardPage.jsx
@@ -1,32 +1,47 @@
 import LeaderboardTable from "../../components/LeaderboardTable/LeaderboardTable";
 
 export const LeaderboardPage = () => {
-    const stats = [{
-        UserId: 1,
-        Username: "Michal",
-        GamesPlayed: 3,
-        GamesWon: 1
+  const stats = [
+    {
+      Username: "Michal",
+      GamesPlayed: 14,
+      GamesWon: 9,
     },
     {
-        UserId: 2,
-        Username: "Jack",
-        GamesPlayed: 2,
-        GamesWon: 1
-    }, {
-        UserId: 3,
-        Username: "Alec",
-        GamesPlayed: 6,
-        GamesWon: 0
-    }, {
-        UserId: 4,
-        Username: "Luke",
-        GamesPlayed: 20,
-        GamesWon: 19
-    }]
-    return (
-        <div>
-        <h1>Leaderboard table</h1>
-        <LeaderboardTable stats={stats}></LeaderboardTable>
-        </div>
-    )
-}
+      Username: "Jack",
+      GamesPlayed: 22,
+      GamesWon: 16,
+    },
+    {
+      Username: "Alec",
+      GamesPlayed: 19,
+      GamesWon: 14,
+    },
+    {
+      Username: "Imogen",
+      GamesPlayed: 8,
+      GamesWon: 4,
+    },
+    {
+      Username: "Abbie",
+      GamesPlayed: 10,
+      GamesWon: 7,
+    },
+    {
+      Username: "Luke",
+      GamesPlayed: 17,
+      GamesWon: 10,
+    },
+    {
+      Username: "Will",
+      GamesPlayed: 3,
+      GamesWon: 1,
+    },
+  ];
+  return (
+    <div>
+      <h1>Leaderboard table</h1>
+      <LeaderboardTable stats={stats}></LeaderboardTable>
+    </div>
+  );
+};
