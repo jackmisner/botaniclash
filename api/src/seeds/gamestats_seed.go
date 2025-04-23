52c925c7a4f13fb742d57d5c282173b8cae26c61
Luke Howeth <LukeHoweth30@gmail.com>
2025-04-23 15:58:04 +0100

    Update seed files to populate game_stats table (to avoid FK_constraint errors)

---
+++ api/src/seeds/gamestats_seed.go
@@ -0,0 +1,39 @@
+package seeds
+
+import (
+	"fmt"
+
+	"github.com/makersacademy/go-react-acebook-template/api/src/models"
+	"gorm.io/gorm"
+)
+
+// GameStatsSeeds seeds the game_stats table with initial stats for each user
+func GameStatsSeeds(db *gorm.DB) {
+	fmt.Println("Seeding game stats...")
+
+	// Get all users to create game stats for them
+	var users []models.User
+	result := db.Find(&users)
+	if result.Error != nil {
+		fmt.Printf("Error fetching users: %v\n", result.Error)
+		return
+	}
+
+	for i, user := range users {
+		// Create game stats with initial values for each user
+		gameStats := models.GameStats{
+			UserID:      user.ID,
+			GamesPlayed: 0,
+			GamesWon:    0,
+		}
+
+		result := db.Create(&gameStats)
+		if result.Error != nil {
+			fmt.Printf("Error creating game stats for user %s: %v\n", user.Username, result.Error)
+		} else {
+			fmt.Printf("Created game stats %d/%d for user: %s\n", i+1, len(users), user.Username)
+		}
+	}
+
+	fmt.Printf("Game stats seeding completed! Created stats for %d users.\n", len(users))
+}
