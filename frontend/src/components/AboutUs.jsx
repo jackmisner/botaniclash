cf0706872e015904f6c77b2511115092d6036f93
Jack Misner <jack.misner@gmail.com>
2025-04-25 10:28:34 +0100

    Made picture part of card be the link to github

--- frontend/src/components/AboutUs.jsx
+++ frontend/src/components/AboutUs.jsx
@@ -6,19 +6,11 @@ const AboutUs = ({ name, github, picture }) => {
     <article className="team-card">
       <p data-testid="common-name">{name}</p>
       <p data-testid="scientific-name">
-        <a href={github} target="_blank" rel="noopener noreferrer">
-          {github.replace("https://github.com/", "@")}
-        </a>
+        {github.replace("https://github.com/", "@")}
       </p>
-      <img
-        data-testid="image-url"
-        src={picture}
-        alt={`${name}'s profile`}
-        onError={(e) => {
-          e.target.src = "/path/to/fallback-image.png";
-          e.target.onerror = null;
-        }}
-      />
+      <a href={github} target="_blank" rel="noopener noreferrer">
+        <img data-testid="image-url" src={picture} alt={`${name}'s profile`} />
+      </a>
     </article>
   );
 };
